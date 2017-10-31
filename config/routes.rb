1a9711ac9c32c588531a847099057f362401572c
root <m.shedd@baypath.net>
2017-10-31 15:22:11 +0000

    fix routes merge conflict

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,10 @@
 Rails.application.routes.draw do
-<<<<<<< HEAD
-  
+
   namespace :cms do
     resources :awards
   end
   get 'awards/index'
-=======
+
 
 
   get 'pages/show'
@@ -18,8 +17,6 @@ Rails.application.routes.draw do
     resources :announcements
   end
   get 'news/index'
-
->>>>>>> d8f0f6496a760e25e7a286c52854a49729de1c15
 
   get 'golf_classic/show'
 
